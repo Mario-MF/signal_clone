51970cd178480f47590f8232a59a6b904dda9b06
Mario-MF <mariomagedfouaad@gmail.com>
2021-01-28 20:15:23 +0200

    second

---
+++ lib/shared/styles/styles.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+TextStyle black14 = TextStyle(
+  fontSize: 14.0,
+);
+
+TextStyle black16 = TextStyle(
+  fontSize: 16.0,
+);
+
+TextStyle black18 = TextStyle(
+  fontSize: 18.0,
+);
+TextStyle black900 = TextStyle(
+  fontSize: 14.0,
+  fontWeight: FontWeight.w500,
+  color: Colors.grey[700],
+);
+TextStyle black20 = TextStyle(
+    fontSize: 18.0,
+
+    fontWeight: FontWeight.w900
+);
+TextStyle black500 = TextStyle(
+    fontSize: 16.0,
+    fontWeight: FontWeight.w500,
+    letterSpacing: 0.3
+);
+
+TextStyle grey14 = TextStyle(
+  fontSize: 14.0,
+  color: Colors.grey[600],
+);
+
+TextStyle grey16 = TextStyle(
+  fontSize: 16.0,
+  color: Colors.grey[600],
+);
+
+TextStyle grey18 = TextStyle(
+  fontSize: 18.0,
+  color: Colors.grey[600],
+);
