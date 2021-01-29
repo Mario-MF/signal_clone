f4fc482b064b2f1663e96c3c0947195c008867aa
Mario-MF <mariomagedfouaad@gmail.com>
2021-01-29 19:15:33 +0200

    add comp

---
+++ lib/shared/components/components.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+Widget defultButton(
+    {@required text,
+      color = Colors.blueAccent,
+      @required function,
+      double r = 5.0,
+      textColor = Colors.white,
+      height,
+      double width = double.infinity,
+      double fz = 15.0,
+      toUpper = true,
+      fontWeight = FontWeight.bold}) =>
+    Container(
+      height: height,
+      width: width,
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.indigo),
+        color: color,
+        borderRadius: BorderRadius.circular(r),
+      ),
+      child: FlatButton(
+        onPressed: function,
+        child: Text(
+          toUpper ? text.toString().toUpperCase() : text.toString(),
+          style: TextStyle(
+            color: textColor,
+            fontSize: fz,
+            fontWeight: fontWeight,
+          ),
+        ),
+        color: color,
+      ),
+    );
+
+void navigateTo({context, widget}) =>
+    Navigator.push(context, MaterialPageRoute(builder: (context) => widget));
