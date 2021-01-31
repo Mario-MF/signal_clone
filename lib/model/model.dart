bc67ff4ea43d98cabab0fa939e6eb63d9478caed
Your Name <you@example.com>
2021-01-31 05:28:45 +0200

    created help screen1

--- lib/model/model.dart
+++ lib/model/model.dart
@@ -1,11 +1,10 @@
-class Categoryconst{
-  int id;
-  String name;
-  String url;
-  Categoryconst({this.id ,this.name,this.url});
-}
-class PopupMenu{
-  var name;
-
-  PopupMenu({this.name});
-}
+// class GetStarted{
+//   int id;
+//   String name;
+//   String url;
+//   GetStarted({this.id ,this.name,this.url});
+// }
+// class PopupMenu{
+//   var name;
+//   PopupMenu({this.name});
+// }
